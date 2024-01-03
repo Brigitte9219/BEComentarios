1dbb2ad39fbb561a3569b85d0da461ba6a829b7e
BrigittePadilla <153017836+BrigittePadilla@users.noreply.github.com>
2024-01-03 10:09:05 -0500

    Generamos Migración - Se realiza el DbContext para representar una sesión de la BD y mapear el modelo con la tabla de la BD..Se configura la cadena de conexión...Se obtiene la obtiene la cadena de conexion para acceder a BD y al politica CORS...Se aplican los comandos para generar la migración hacia la BD

---
+++ BEComentarios/AplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BEComentarios.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BEComentarios
+{
+    public class AplicationDbContext : DbContext //Representa una sesión de la BD (almacenar datos, hacer querys...)
+    {
+        public DbSet<Comentario> Comentario { get; set; } //Estamos mapeando el modelo con la tabla de la BD
+
+        public AplicationDbContext(DbContextOptions<AplicationDbContext>options): base(options) //constructor
+        {
+        
+        }
+    }
+}
--- BEComentarios/Program.cs
+++ BEComentarios/Program.cs
@@ -1,6 +1,25 @@
+using BEComentarios;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
+//Obtiene la cadena de conexion "DefaultConnection"
+var conexion = builder.Configuration.GetConnectionString("DefaultConnection");
+
 // Add services to the container.
+
+//Permite acceder a la BD
+builder.Services.AddDbContext<AplicationDbContext>(options => options.UseMySql(conexion, ServerVersion.AutoDetect(conexion)));
+
+//Configura la política CORS (Seguridad) permitiendo cualquier origen, método y encabezado
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy",
+    builder => builder.AllowAnyOrigin()
+    .AllowAnyMethod()
+    .AllowAnyHeader());
+});
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
